309613ec3d448370e748a99218abfadecdaef3ea
sbx_user1060 <sbx_user1060@ip-10-129-83-87.eu-west-2.compute.internal>
2019-02-07 22:16:01 +0000

    Thu Feb 07 22:16:01 UTC 2019

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -27,19 +27,7 @@ public class CheckoutSolution {
 	
 	
 	
-	private final class MultiOffer implements Offer {
-		@Override
-		public Basket discount(Basket basket) {
-			long quantity = basket.quantities.getOrDefault("E", 0L);
-			if(quantity < 2L || !basket.quantities.containsKey("B")) {
-				return basket;
-			}
-			int free = (int) (quantity / 2);
-			HashMap<String, Long> newContents = new HashMap<>(basket.quantities);
-			newContents.put("B", Math.max(newContents.get("B") - free, 0)); 
-			return new Basket(newContents, basket.total);
-		}
-	}
+	
 
 	final Map<String, Integer> stockPrice;
 	final List<Offer> offers;
--- src/main/java/befaster/solutions/CHK/MultiOffer.java
+++ src/main/java/befaster/solutions/CHK/MultiOffer.java
@@ -1,14 +1,33 @@
 package befaster.solutions.CHK;
+
+import java.util.HashMap;
+
 public final class MultiOffer implements Offer {
-		@Override
-		public Basket discount(Basket basket) {
-			long quantity = basket.quantities.getOrDefault("E", 0L);
-			if(quantity < 2L || !basket.quantities.containsKey("B")) {
-				return basket;
-			}
-			int free = (int) (quantity / 2);
-			HashMap<String, Long> newContents = new HashMap<>(basket.quantities);
-			newContents.put("B", Math.max(newContents.get("B") - free, 0)); 
-			return new Basket(newContents, basket.total);
-		}
+	
+	
+	private String givenSku;
+	private int givenQuantity;
+	private String applyToSku;
+	private int applyToQuantity;
+
+
+	public MultiOffer(String givenSku, int givenQuantity, String applyToSku, int applyToQuantity) {
+		this.givenSku = givenSku;
+		this.givenQuantity = givenQuantity;
+		this.applyToSku = applyToSku;
+		this.applyToQuantity = applyToQuantity;
 	}
+	
+	
+	@Override
+	public Basket discount(Basket basket) {
+		long quantity = basket.quantities.getOrDefault(givenSku, 0L);
+		if (quantity < givenQuantity || !basket.quantities.containsKey(applyToSku)) {
+			return basket;
+		}
+		int free = (int) (quantity / givenQuantity) * applyToQuantity;
+		HashMap<String, Long> newContents = new HashMap<>(basket.quantities);
+		newContents.put(applyToSku, Math.max(newContents.get(applyToSku) - free, 0));
+		return new Basket(newContents, basket.total);
+	}
+}
