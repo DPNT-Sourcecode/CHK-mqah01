40861e254b4e6fd62199197fb6b9daee7b20d634
sbx_user1060 <sbx_user1060@ip-10-129-83-87.eu-west-2.compute.internal>
2019-02-07 19:28:44 +0000

    Thu Feb 07 19:28:44 UTC 2019

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -1,9 +1,44 @@
 package befaster.solutions.CHK;
 
-import befaster.runner.SolutionNotImplementedException;
+import static java.util.Collections.emptyMap;
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+/**
++------+-------+----------------+
+| Item | Price | Special offers |
++------+-------+----------------+
+| A    | 50    | 3A for 130     |
+| B    | 30    | 2B for 45      |
+| C    | 20    |                |
+| D    | 15    |                |
++------+-------+----------------+
+
+ *
+ */
 public class CheckoutSolution {
+	
+	
     public Integer checkout(String skus) {
-        throw new SolutionNotImplementedException();
+    	
+    	Map<String, Long> quantities = quantities(skus);
+    	
+    	
     }
+
+	Map<String, Long> quantities(String skus) {
+		return (skus == null) ? emptyMap() : 
+			Arrays.stream(skus.split("[\\s,]+"))
+				.collect(groupingBy(identity(), counting()));
+	}
 }
+
