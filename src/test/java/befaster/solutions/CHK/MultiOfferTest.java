821613b52a1ddf5388cfe3d8351b2bfae1f094b5
sbx_user1060 <sbx_user1060@ip-10-129-83-87.eu-west-2.compute.internal>
2019-02-07 22:22:01 +0000

    Thu Feb 07 22:22:01 UTC 2019

--- src/test/java/befaster/solutions/CHK/MultiOfferTest.java
+++ src/test/java/befaster/solutions/CHK/MultiOfferTest.java
@@ -1,5 +1,25 @@
 package befaster.solutions.CHK;
 
+import static java.util.Collections.singletonMap;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+import org.junit.Test;
+
 public class MultiOfferTest {
 
+	MultiOffer offer = new MultiOffer("F", 2, "F", 1);
+	
+	@Test
+	public void twoForOne() {
+		
+		Basket basket = new Basket(singletonMap("F", 2L), 0);
+		Basket discounted = offer.discount(basket);
+		
+		assertThat(discounted.quantities, allOf(
+				is(aMapWithSize(1)),
+				hasEntry("F", 1L)
+		));
+	}
 }
+
