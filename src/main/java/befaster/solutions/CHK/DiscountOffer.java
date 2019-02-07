6e2271410fa59f96be8d739291bddfb4310c0c62
sbx_user1060 <sbx_user1060@ip-10-129-83-87.eu-west-2.compute.internal>
2019-02-07 22:54:36 +0000

    Thu Feb 07 22:54:36 UTC 2019

--- src/main/java/befaster/solutions/CHK/DiscountOffer.java
+++ src/main/java/befaster/solutions/CHK/DiscountOffer.java
@@ -1,16 +1,30 @@
 package befaster.solutions.CHK;
 
+import java.util.HashMap;
 
-
-
-public class DiscountOffer extends AbstractDiscountOffer {
+public class DiscountOffer implements Offer {
 	String sku;
+	int quantity;
+	int price;
 
 	public DiscountOffer(String sku, int quantity, int price) {
 		this.sku = sku;
 		this.quantity = quantity;
 		this.price = price;
 	}
+
+	@Override
+	public Basket discount(Basket basket) {
+		Long quantity = basket.quantities.getOrDefault(sku, 0L);
+		if (quantity == 0L) {
+			return basket;
+		}
+		int actual = quantity.intValue() / this.quantity;
+		HashMap<String, Long> newContents = new HashMap<>(basket.quantities);
+		newContents.put(sku, quantity % this.quantity);
+		return new Basket(newContents, basket.total + (actual * price));
+	}
 }
+
 
 
--- src/main/java/befaster/solutions/CHK/MultiDiscountOffer.java
+++ src/main/java/befaster/solutions/CHK/MultiDiscountOffer.java
@@ -1,9 +1,30 @@
 package befaster.solutions.CHK;
 
-public class MultiDiscountOffer extends DiscountOffer {
+import java.util.HashMap;
+import java.util.Set;
 
-	public MultiDiscountOffer(String sku, int quantity, int price) {
-		super(sku, quantity, price);
+public class MultiDiscountOffer implements Offer {
+
+	Set<String> skus;
+	int quantity;
+	int price;
+
+	public MultiDiscountOffer(Set<String> skus, int quantity, int price) {
+		this.skus = skus;
+		this.quantity = quantity;
+		this.price = price;
 	}
 
+	@Override
+	public Basket discount(Basket basket) {
+		Long quantity = basket.quantities.getOrDefault(sku, 0L);
+		if (quantity == 0L) {
+			return basket;
+		}
+		int actual = quantity.intValue() / this.quantity;
+		HashMap<String, Long> newContents = new HashMap<>(basket.quantities);
+		newContents.put(sku, quantity % this.quantity);
+		return new Basket(newContents, basket.total + (actual * price));
+	}
 }
+
